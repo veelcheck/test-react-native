f0953f3d961d33d7eb2d163c929aaaf5c52bdac6
veelcheck <veelcheck@gmail.com>
2025-06-11 07:00:01 +0200

    setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="text-green-700">
+      <Text>Veelcheck</Text>
+    </View>
+  );
+}
